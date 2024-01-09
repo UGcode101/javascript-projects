2f2fbff9d5d347695d7f96ec7884a46b493d3d88
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-01-08 20:38:23 -0600

    1st hw

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -8,4 +8,20 @@
 
 // Calculate a trip to the moon below
 
-// Print the results of the trip to the moon below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKilometer = .621
+console.log (typeof shuttleName)
+console.log (typeof shuttleSpeedMph)
+console.log (typeof distanceToMarsKm)
+console.log (typeof distanceToMoonKm)
+let milesToMars = distanceToMarsKm * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
+console.log (shuttleName + " will take " + daysToMars + " days to reach Mars.");
+let milesToMoon = distanceToMoonKm * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+console.log (shuttleName + " will take " + daysToMoon + " days to reach Moon.")
