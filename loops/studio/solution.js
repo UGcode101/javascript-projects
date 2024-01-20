4be5d9d155c87798016547071fc7f69ebda7c931
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-01-20 14:24:37 -0600

    studio complete

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -25,10 +25,23 @@ let desserts = [
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
+}
+/// Part A #2: Write a ``for`` loop inside this function
+/// Code your solution for part A #2 below this comment (and above the return statement) ... ///
 
-  /// Part A #2: Write a ``for`` loop inside this function
-  /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
+  let pantry = [protein, grains, veggies, beverages, desserts];
+  let meals = [];
 
+  for (let i = 0; i < numMeals; i++) {
+    let meal = [];
+    for (let j = 0; j < pantry.length; j++) {
+      let category = pantry[j];
+      let randomItem = category[Math.floor(Math.random() * category.length)];
+      meal.push(randomItem);
+    }
+    meals.push(meal);
+  }
   return meals;
 }
 
@@ -36,6 +49,11 @@ function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
 
   /// CODE YOUR SOLUTION TO PART B here ///
+
+  function askForNumber() {
+    let numMeals = input.question("How many meals would you like to make?");
+    return parseInt(numMeals);
+  }
 
   return numMeals;
 }
@@ -45,6 +63,25 @@ function generatePassword(string1, string2) {
 
   /// Code your Bonus Mission Solution here ///
 
+  function generatePassword(string1, string2) {
+    let code = "";
+
+    // Determine the length of the longer string
+    let maxLength = Math.max(string1.length, string2.length);
+
+    // Iterate through both strings alternately
+    for (let i = 0; i < maxLength; i++) {
+      if (i < string1.length) {
+        code += string1[i];
+      }
+      if (i < string2.length) {
+        code += string2[i];
+      }
+    }
+
+    return code;
+  }
+
   return code;
 }
 
@@ -53,19 +90,28 @@ function runProgram() {
   /// UNCOMMENT the two lines of code below that invoke the mealAssembly function (starting with 'let meals =') and print the result ///
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals);
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(
+    protein,
+    grains,
+    veggies,
+    beverages,
+    desserts,
+    askForNumber()
+  );
+  console.log(mealsForX);
   /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  let password1 = "";
+  let password2 = "";
+  console.log(
+    "Time to run the password generator so we can update the menu tomorrow."
+  );
+  console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
