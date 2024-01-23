10d69321f6d53368f0a3ffba6da2ba44e6e9249f
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-01-23 12:54:33 -0600

    tried creating string without template literal

--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -14,12 +14,12 @@ console.log(cargoHold);
 
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
 const removedItem = cargoHold.pop();
-console.log(`Element removed: ${removedItemPop}`);
+console.log("Element removed: removedItemPop");
 console.log(cargoHold);
 
 //3) Remove the first item from the array with shift. Print the element removed and the updated array.
 const removedItemShift = cargoHold.shift();
-console.log(`Element removed (shift): ${removedItemShift}`);
+console.log("Element removed (shift): " + removedItemShift);
 console.log(cargoHold);
 
 //4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
