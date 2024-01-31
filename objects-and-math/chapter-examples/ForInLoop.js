b3b8f34524b7e6ff3daaff515999032efdd8e96c
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-01-30 19:53:18 -0600

    finish chapter ex.

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -1,9 +1,11 @@
 let tortoiseOne = {
-   species: "Galapagos Tortoise",
-   name: "Pete",
-   weight: 919,
-   age: 85,
-   diet: ["pumpkins", "lettuce", "cabbage"]
+  species: "Galapagos Tortoise",
+  name: "Pete",
+  weight: 919,
+  age: 85,
+  diet: ["pumpkins", "lettuce", "cabbage"],
 };
-
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+for (let key in tortoiseOne) {
+  console.log(tortoiseOne[key]);
+}
--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -1,17 +1,48 @@
-function randomSelection(arr){
-   let index = Math.floor(Math.random()*arr.length);
-   return arr[index];
- }
- 
- let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
- 
- let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
- 
- for (i=0; i < 8; i++){
-   console.log(randomSelection(happiness));
- }
- 
- //Experiment with the code above. Try to:
- //a) Print 3 random selections from each array.
- //b) Have the code randomly pick one array, and then print 2 random items from it.
- //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+function randomSelection(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+let newArray = [];
+
+let happiness = [
+  "Hope",
+  "Joy",
+  "Peace",
+  "Love",
+  "Kindness",
+  "Puppies",
+  "Kittens",
+  "Tortoise",
+];
+
+let words = ["Hello", "World", "Python", "JavaScript", "Rutabaga"];
+
+for (i = 0; i < 8; i++) {
+  console.log(randomSelection(happiness));
+}
+
+//Experiment with the code above. Try to:
+//a) Print 3 random selections from each array.
+console.log("3 random selections from happiness:");
+for (let i = 0; i < 3; i++) {
+  console.log(randomSelection(happiness));
+}
+
+console.log("3 random selections from words:");
+for (let i = 0; i < 3; i++) {
+  console.log(randomSelection(words));
+}
+
+//b) Have the code randomly pick one array, and then print 2 random items from it.
+let randomArray = randomSelection([happiness, words]);
+console.log("Random array selected:", randomArray);
+
+console.log("2 random items from the selected array:");
+for (let i = 0; i < 2; i++) {
+  console.log(randomSelection(randomArray));
+}
+
+//c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+newArray.push(randomSelection(happiness));
+newArray.push(randomSelection(words));
+console.log("New array filled with random items:", newArray);
