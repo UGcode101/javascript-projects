5cba577268639de97d4f8270723192468d897773
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-02-02 20:20:07 -0600

    complete

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -1,8 +1,8 @@
 //Import modules:
-const input = require(readline - sync);
-const averages = require(modules / exercises / ScoreCalcs / averages.js);
-const printAll = require(display.js);
-const randomSelect = require(randomSelect.js);
+const input = require("readline-sync");
+const averages = require("./ScoreCalcs/averages.js");
+const printAll = require("./display.js");
+const randomSelect = require("./randomSelect.js");
 
 //Candidate data:
 let astronauts = ["Fox", "Turtle", "Cat", "Hippo", "Dog"];
@@ -29,22 +29,23 @@ for (let i = 0; i < prompts.length; i++) {
   let response = input.question(`Would you like to ${prompts[i]}? Y/N: `);
   if (response.toLowerCase() === "y") {
     if (i === 0) {
-      //Call 'printAll' here and pass in all necessary arguments.
+      printAll(astronauts, testTitles, scores);
     } else if (i === 1) {
       for (let j = 0; j < testTitles.length; j++) {
-        let avg = //Call 'averageForTest' here. Pass in j and scores as arguments.
-          console.log(`${testTitles[j]} test average = ${avg}%.`);
+        let avg = averages.averageForTest(j, scores);
+        console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i === 2) {
       for (let j = 0; j < astronauts.length; j++) {
-        let avg = //Call 'averageForStudent' here. Pass in j and scores as arguments.
-          console.log(`${astronauts[j]}'s test average = ${avg}%.`);
+        let avg = averages.averageForStudent(j, scores);
+        console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = //Call 'randomSelect' to pick a spacewalker from the astronauts array.
-        console.log(`${walker} is the next spacewalker.`);
+      let walker = randomSelect(astronauts);
+      console.log(`${walker} is the next spacewalker.`);
     }
   } else {
     console.log("Option skipped.");
   }
 }
+printAll(astronauts, testTitles, scores);
