31f07dbba43076996625e20fb7a4d52633a97b0c
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-02-12 21:23:08 -0600

    part 1

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,9 +1,19 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate {
+  constructor(name, mass, scores) {
+    this.name = name;
+    this.mass = name;
+    this.scores = scores;
+  }
+}
+let bubbaBear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+let merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+let gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
 
-
+console.log("Bubba Bear:", bubbaBear);
+console.log("Merry Maltese:", merryMaltese);
+console.log("Glad Gator:", gladGator);
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
-
-
 
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
