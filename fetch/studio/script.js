148d9405e662dafca3c22d47183342dfd48e1689
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-03-04 19:56:55 -0600

    complete studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,34 @@
-//TODO: Add Your Code Below
+window.addEventListener("load", function () {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((astronauts) => {
+      const container = document.getElementById("container");
+      let astronautsHTML = "";
+      for (let i = 0; i < astronauts.length; i++) {
+        const astronaut = astronauts[i];
+        astronautsHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${
+                              astronaut.active
+                                ? '<span style="color: green;">Yes</span>'
+                                : "No"
+                            }</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}">
+                </div>
+            `;
+      }
+      container.innerHTML = astronautsHTML;
+    });
+});
